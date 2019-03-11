3a54518f7c3b6b27711605451a39419a344abe27
BTchorzewski <tchorzewskibartosz@gmail.com>
2019-03-11 19:55:51 +0100

    dodano owlcarousel

---
+++ scripts/owl-carousel.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".carousel1").owlCarousel({
+        loop: true,
+        autoplay: true,
+        center: true,
+        mouseDrag: true,
+        nav: true,
+        dots: false,
+        items: 1
+    });
+    $(".carousel2").owlCarousel({
+        loop: true,
+        autoplay: true,
+        center: true,
+        mouseDrag: true,
+        nav: true,
+        dots: false,
+        items: 1
+    });
+});
