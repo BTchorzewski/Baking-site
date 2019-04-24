8269ae0ce0d195235dd864a80a9662c292897d7b
BTchorzewski <tchorzewskibartosz@gmail.com>
2019-04-24 22:28:07 +0200

    added animation to navigation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     $('.navigation__burger').click(function(){
         $('.navigation__list').toggleClass('navigation__list--open');
-    })
+    });
     $('.navigation__link').click(function(){
         $('.navigation__list').removeClass('navigation__list--open');
-    })
-})
+        $('.navigation__burger').removeClass('navigation__burger--open');
+    });
+    $('.navigation__burger').click(function(){
+        $(this).toggleClass('navigation__burger--open');
+    });
+});
