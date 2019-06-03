a10431ddfb4cc75c73c8b2a8f5480cb5215dba84
BTchorzewski <tchorzewskibartosz@gmail.com>
2019-06-03 21:24:48 +0200

    added popups

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     });
     $(".carousel2").owlCarousel({
         loop: true,
-        autoplay: false,
+        autoplay: true,
         center: true,
         mouseDrag: true,
         nav: true,
